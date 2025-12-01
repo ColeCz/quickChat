ae2a9ee527f040e8b0189aa14743318d58942978
ColeCz <czekalacole@gmail.com>
2025-11-30 22:56:57 -0500

    update schema syntax

--- database/02-create-schema.sql
+++ database/02-create-schema.sql
@@ -1,27 +1,26 @@
+CREATE TABLE users (
+    username VARCHAR(64) PRIMARY KEY,
+    passwrd VARCHAR(64) NOT NULL
+);
 
-CREATE TABLE user {
-    username VARCHAR(64) PRIMARY KEY;
-    passwrd VARCHAR(64) NOT NULL;
-}
+CREATE TABLE conversation (
+    convo_id SERIAL PRIMARY KEY
+);
 
-CREATE TABLE conversation {
-    convo_id SERIAL PRIMARY KEY;
-}
+CREATE TABLE conversation_recipients (
+    username VARCHAR(64),
+    convo_id INT,
+    FOREIGN KEY (username) REFERENCES users(username),
+    FOREIGN KEY (convo_id) REFERENCES conversation(convo_id),
+    PRIMARY KEY (username, convo_id)
+);
 
-CREATE TABLE conversation_recipients {
-    username VARCHAR(64);
-    convo_id INT;
-    FOREIGN KEY username REFERENCES user (username);
-    FOREIGN KEY convo_id REFERENCES conversation (convo_id);
-    PRIMARY KEY (username, convo_id);
-}
-
-CREATE TABLE message {
-    message_id SERIAL PRIMARY KEY;
-    message_data TEXT NOT NULL;
-    message_timestamp TIMESTAMP DEFAULT NOW();
-    sender VARCHAR(64) NOT NULL;
-    convo_id INT NOT NULL;
-    FOREIGN KEY sender REFERENCES user (username);
-    FOREIGN KEY convo_id REFERENCES conversation (convo_id);
-}
+CREATE TABLE message (
+    message_id SERIAL PRIMARY KEY,
+    message_data TEXT NOT NULL,
+    message_timestamp TIMESTAMP DEFAULT NOW(),
+    sender VARCHAR(64) NOT NULL,
+    convo_id INT NOT NULL,
+    FOREIGN KEY (sender) REFERENCES users(username),
+    FOREIGN KEY (convo_id) REFERENCES conversation(convo_id)
+);
